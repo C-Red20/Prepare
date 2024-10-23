e33211db0669d6674df93544faf3f0334ecd89fb
C-Red20 <caila.linger@yahoo.com>
2024-10-22 23:09:56 -0400

    ???

--- Prepare/Prepare/client/prep/src/Managers/ListItemManger.jsx
+++ Prepare/Prepare/client/prep/src/Managers/ListItemManger.jsx
@@ -1,58 +1,34 @@
 const apiUrl = "https://localhost:7254/api/ListItem";
 
 export const updateListItem = async ({ itemId, amount }) => {
-  try {
-    const response = await fetch(`${apiUrl}/${itemId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ amount }),
-    });
-    if (!response.ok) {
-      throw new Error("Failed to update list item");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error updating list item:", error);
-    throw error;
-  }
+  const response = await fetch(`${apiUrl}/item/${itemId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ amount }),
+  });
+  return await response.json();
 };
 
 export const deleteListItem = async (itemId) => {
-  try {
-    const response = await fetch(`${apiUrl}/${itemId}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      throw new Error(`Failed to delete list item with id ${itemId}`);
-    }
-  } catch (error) {
-    console.error("Error deleting list item:", error);
-    throw error;
-  }
+  const response = await fetch(`${apiUrl}/${itemId}`, {
+    method: "DELETE",
+  });
 };
 
 export const getListItemsByListId = async (listId) => {
-  return fetch(`${apiUrl}/list/${listId}`)
-  .then((res) => res.json());
-}
+  const response = await fetch(`${apiUrl}/list/${listId}`);
+  return await response.json();
+};
 
 export const addItemToList = async (item) => {
-  try {
-    const response = await fetch(`${apiUrl}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(item),
-    });
-    if (!response.ok) {
-      throw new Error("Failed to add item to list");
-    }
-    return await response.json();
-  } catch (error) {
-    console.error("Error adding item to list:", error);
-    throw error;
-  }
+  const response = await fetch(`${apiUrl}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(item),
+  });
+  return await response.json();
 };
